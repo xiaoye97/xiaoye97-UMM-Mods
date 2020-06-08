38aebd16723e9c410737aa6401b667ea87861668
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2020-06-09 00:17:58 +0800

    添加Mod
    添加 王国双冠 可以拆除墙Mod

---
+++ KingdomTwoCrowns/CanDestroyWall/CanDestroyWall/Main.cs
@@ -0,0 +1,72 @@
+﻿using Harmony12;
+using UnityEngine;
+using Coatsink.Common;
+using System.Reflection;
+using UnityModManagerNet;
+using System.Collections.Generic;
+
+namespace me.xiaoye97.UMMMod.KingdomTwoCrowns.CanDestroyWall
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry mod;
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            mod = modEntry;
+            mod.OnGUI = OnGUI;
+            HarmonyInstance.Create(mod.Info.Id).PatchAll(Assembly.GetExecutingAssembly());
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginVertical("可以拆除墙", GUI.skin.window, new GUILayoutOption[0]);
+            GUILayout.Label("站在墙的中间，按C键，可以消耗1金币拆除当前墙壁。", new GUILayoutOption[0]);
+            GUILayout.Label("作者:xiaoye97 QQ:1066666683", new GUILayoutOption[0]);
+            GUILayout.EndVertical();
+        }
+
+        public static void RemoveWall()
+        {
+            Kingdom kingdom = SingletonMonoBehaviour<Managers>.Inst.kingdom;
+            if (kingdom.playerOne != null)
+            {
+                if (kingdom.playerOne.coins > 0)
+                {
+                    var walls = Traverse.Create(kingdom).Field("_walls").GetValue<HashSet<Wall>>();
+                    Wall wall = null;
+                    float min = float.MaxValue;
+                    walls.Do((w) =>
+                    {
+                        float dis = Vector3.Distance(w.transform.position, kingdom.playerOne.transform.position);
+                        if (dis < 1.4f && dis < min)
+                        {
+                            wall = w;
+                            min = dis;
+                        }
+                    });
+                    if (wall != null)
+                    {
+                        Traverse.Create(wall).Field("_damageable").GetValue<Damageable>().ReceiveDamage(999, kingdom.playerOne.gameObject, DamageSource.Knight);
+                        kingdom.playerOne._wallet.coins--;
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Kingdom), "Update")]
+        class InputPatch
+        {
+            public static void Postfix()
+            {
+                if (mod.Enabled)
+                {
+                    if (Input.GetKeyDown(KeyCode.C))
+                    {
+                        RemoveWall();
+                    }
+                }
+            }
+        }
+    }
+}
